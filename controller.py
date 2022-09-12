488a3d7c44971e6bd912c385c4a0520410994516
Mograa <95922954+Mograa@users.noreply.github.com>
2022-09-12 16:21:05 -0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,7 +1,8 @@
-import webbrowser
 import main
+from APIs.weather_api import temp, temperature
+from APIs.nasa_api import imgtoday
 import wikipedia
-import requests as req
+from playsound import playsound
 
 try:
     def exec():
@@ -13,43 +14,35 @@ try:
         # musics commands
         elif main.spc == main.data_music1:
             print('playing "De Kenner" - LEOD')
-            main.playsound.playsound(r'./sounds/music/de_kenner.mp3')
+            playsound.playsound(r'./sounds/music/de_kenner.mp3')
         elif main.spc == main.data_music2:
             print('playing "Poze indie" - LEOD')
-            main.playsound.playsound(r'./sounds/music/poze_indie.mp3')
+            playsound.playsound(r'./sounds/music/poze_indie.mp3')
         elif main.spc == main.data_music3:
             print('playing "Hotline chique" - LEOD')
-            main.playsound.playsound(r'./sounds/music/HOTLINE_MUSIC.mp3')
+            playsound.playsound(r'./sounds/music/HOTLINE_MUSIC.mp3')
         elif main.spc == main.data_music4:
             print('playing "Se ta solteira" - LEOD')
-            main.playsound.playsound(r'./sounds/music/SE_TA_SOLTEIRA.mp3')
+            playsound.playsound(r'./sounds/music/SE_TA_SOLTEIRA.mp3')
         elif main.spc == main.data_lofi:
             print('Playing LOFI compilation')
-            main.playsound.playsound(r'./sounds/music/lofi.mp3')
+            playsound.playsound(r'./sounds/music/lofi.mp3')
 
         # temperature command
         elif main.data_temperature in main.spc:
-            search_temp = main.spc.replace('Maria qual é a temperatura em', '')
-            API_KEY = 'cb3d4b7abf1326d2ba1520686ee7c8c8'
-            city = search_temp
-            api_link = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
-            json_req = req.get(api_link).json()
-            temp_city = json_req['main']['temp'] - 273.115
-            temp_formatted = f'{temp_city:.0f} Graus'
-            main.machine.say(temp_formatted)
-            main.machine.runAndWait()
-
+            temperature()
+            
         # search youtube command
         elif main.data_youtube in main.spc:
             search_youtube = main.spc.replace('Maria Pesquise por', '')
             link = f'https://www.youtube.com/results?search_query={search_youtube}'
-            webbrowser.open(link)
+            main.webbrowser.open(link)
 
         # search google command
         elif main.data_google in main.spc:
             search_google = main.spc.replace('Maria busque por', '')
             link = f'https://www.google.com/search?q={search_google}'
-            webbrowser.open(link)
+            main.webbrowser.open(link)
 
         # search wikipedia command
         elif main.data_wiki in main.spc:
@@ -57,15 +50,12 @@ try:
             wikipedia.set_lang('pt')
             result = wikipedia.summary(search, 2)
             link = "wikipedia.org/wiki/"
-            webbrowser.open(link + search)
+            main.webbrowser.open(link + search)
             main.machine.say(result)
             main.machine.runAndWait()
-
-        # joke command
-        elif main.spc == main.data_joke:
-            joke = 'Qual o animal que não vale nada. O javali'
-            main.machine.say(joke)
-            main.machine.runAndWait()
-    exec()
+            
+        # space image command
+        elif main.spc == main.data_space:
+            imgtoday()
 except:
-    print('Error in controller archive')
+    pass
