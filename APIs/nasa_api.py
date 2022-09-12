2197fa6c4c52988d4a08167a533a51926fdd038a
Mograa <95922954+Mograa@users.noreply.github.com>
2022-09-12 16:22:40 -0300

    Add files via upload

---
+++ APIs/nasa_api.py
@@ -0,0 +1,9 @@
+import webbrowser
+from requests import get
+
+def imgtoday():
+    API_KEY = '2J5KBhpGWy3goE1wQ5ZYDD3dZU0a5GZZkOALpyoD'
+    api_req = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
+    req_json = get(api_req).json()
+    req_img = req_json['hdurl']
+    webbrowser.open(req_img)
